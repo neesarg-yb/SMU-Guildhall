1038f9486e96b090c53c144c062e3d5f87a20ea7
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2017-02-21 16:05:12 +0530

    A2: Tested for various file
    Working fine..

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  ofstream fout(outFile, ios::binary);
+  ofstream fout(outFile, ios::binary | ios::trunc);
   if(!fout.good()) {
     cout<<"ERROR: can not open \""<<outFile<<"\" file.."<<endl;
     return 1;
